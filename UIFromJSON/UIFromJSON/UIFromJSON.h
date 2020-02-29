16a92644f6554a07327fccf37901891c78b6ffdf
Regan Russell <regan.russell@gmail.com>
2020-03-01 03:45:09 +1100

    Initial commit

---
+++ UIFromJSON/UIFromJSON/UIFromJSON.h
@@ -0,0 +1,19 @@
+//
+//  UIFromJSON.h
+//  UIFromJSON
+//
+//  Created by Regan Russell on 1/3/20.
+//  Copyright © 2020 PymbleSoftware PtyLtd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIFromJSON.
+FOUNDATION_EXPORT double UIFromJSONVersionNumber;
+
+//! Project version string for UIFromJSON.
+FOUNDATION_EXPORT const unsigned char UIFromJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIFromJSON/PublicHeader.h>
+
+
